916fc8fb3a310d75d7e23feb2efe8dc249f7ae7d
Richard <richard.larsen@students.cdu.edu.au>
2026-04-12 12:50:13 +0930

    Update HIT137_Assign2_EXT21_Question1.py

--- HIT137_Assign2_EXT21_Question1.py
+++ HIT137_Assign2_EXT21_Question1.py
@@ -27,49 +27,54 @@ def encrypt_file(initial_data, path):
         return index_letter
     s1 = 14
     s2 = 14
-    while not(s1 <= 12 and s2 <=12):
+    while not(s1 <= 12 and s2 <=12):                                    #input values for encrytion mechanic
         s1 = int(input("Enter the first amount of alphabetical letter shifts for this encryption :"))
         if not(s1 <=12) or s1 < 0:
             print("Error - Out of shift range enter a number between 1 and 12")
         s2 = int(input("Enter the second amount of alphabetical letter shifts for this encryption :"))
         if not(s2 <=12) or s2 < 0:
             print("Error - Out of shift range, enter a number between 1 and 12")      
-    enc_lower1 = [chr(i) for i in range(ord('a'), ord('m') + 1)]
-    enc_lower2 = [chr(i2) for i2 in range(ord('A'), ord('M') + 1)]
-    enc_upper1 = [chr(i3) for i3 in range(ord('n'), ord('z') + 1)]
-    enc_upper2 = [chr(i4) for i4 in range(ord('N'), ord('Z') + 1)]
+    enc_lower1 = [chr(i) for i in range(ord('a'), ord('m') + 1)]        #create a string of letters from a to m
+    full_alpha_lower = [chr(i) for i in range(ord('a'), ord('z') + 1)]  #create a string of letters from a to z
+    enc_lower2 = [chr(i2) for i2 in range(ord('A'), ord('M') + 1)]      #create a string of letters from A to M
+    enc_upper1 = [chr(i3) for i3 in range(ord('n'), ord('z') + 1)]      #create a string of letters from n to z
+    full_alpha_upper = [chr(i) for i in range(ord('A'), ord('Z') + 1)]  #create a string of letters from A to Z
+    enc_upper2 = [chr(i4) for i4 in range(ord('N'), ord('Z') + 1)]      #create a string of letters from N to Z
     
-    encrypted_string = []
-    for char in initial_data:
-        if char.isalpha():
-            if char in enc_lower1:
-                index = find_letter(enc_lower1,char)                #encrypt lowercase a to m method
-                if (index + (s1*s2)) >= 12:
-                    new_char = "m"
+    encrypted_string = []                                               #initialise variable to hold the results
+    for char in initial_data:                                           #variable holds check value from input
+        if char.isalpha():                                              #check for aphabetical character
+            if char in enc_lower1:                                      #check value is in range a to m
+                index = find_letter(full_alpha_lower,char)                #encrypt lowercase a to m method
+                if (index + (s1*s2)) >= 25:
+                    new_char = "z"
                 else:
-                    new_char = enc_lower1[index + (s1 *s2)]
-            if char in enc_lower2:
-                index = find_letter(enc_lower2,char)                #encrypt uppercase A to M method
+                    new_char = full_alpha_lower[index + (s1 *s2)]
+            if char in enc_lower2:                                      #check letter is in range A to M
+                index = find_letter(full_alpha_upper,char)                #encrypt uppercase A to N method
                                        
                 if (index - (s1+s2)) < 0:
                     new_char = "A"
                 else:
-                    new_char = enc_lower2[index - (s1+s2)]
-            if char in enc_upper1:   
-                index = find_letter(enc_upper1,char)                #encrypt lowercase n to z method
+                    new_char = full_alpha_upper[index - (s1+s2)]
+            if char in enc_upper1:                                      #check letter is in range n to z
+                index = find_letter(full_alpha_lower,char)                #encrypt lowercase n to z method
                 if (index -s1) <= 0:
-                    new_char = "n"
+                    new_char = "a"
                 else:
-                    new_char = enc_upper1[index-s1]            
-            if char in enc_upper2:  
-                index = find_letter(enc_upper2,char)                 #encrypt uppercase N to Z method
-                if (index + (s2*s2)) >= 12:
+                    new_char = full_alpha_lower[index-s1]            
+            if char in enc_upper2:                                      #check letter is in range N to Z
+                index = find_letter(full_alpha_upper,char)                 #encrypt uppercase M to Z method
+                if (index + (s2*s2)) >= 25:
                     new_char = "Z"
                 else:
-                    new_char = enc_upper2[index +(s2*s2)]  
+                    new_char = full_alpha_upper[index +(s2*s2)]  
               
         else:
-            new_char = char
-        encrypted_string.append(new_char)
-    return encrypted_string 
+            new_char = char                                             #check value is not alphabetical save value
+        encrypted_string.append(new_char)                               #add current vale to end of string
+    return encrypted_string                                             #return encrypted string
+    
+                                                             
 main()
+  
